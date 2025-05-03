f32bf769b457ffb3f4d79e65aa64944187e6f8d6
Lincoln-Hernandes2024-univesp <2209236@aluno.univesp.br>
2025-05-03 17:48:14 -0300

    removing unused variable 'tmp'

--- src/routes/perfil/perfil_loader.ts
+++ src/routes/perfil/perfil_loader.ts
@@ -9,8 +9,6 @@ export default async function Loader() {
 
         if(user) {
 
-            let tmp:any = Buffer.from(user[1], 'base64').toJSON()
-
             return JSON.parse(window.atob(user[1])).sub
         }
         throw new Error()
